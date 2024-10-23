afe25d42aa302ae0d1927b8100d4d04a78f1d695
vedashreeath <36235092+vedashreeath@users.noreply.github.com>
2024-10-23 14:29:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,40 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "aws_eks_cluster" "my_cluster" {
-  name = "my-eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  name     = "my-eks-cluster"
+  role_arn = "arn:aws:iam::047719656762:role/eks-role"
 
   vpc_config {
-    subnet_ids = aws_subnet.eks_subnet[*].id
+    subnet_ids = [
+      "subnet-0670bc5b424180d00",
+      "subnet-01d3f9c3ffd4694a4",
+      "subnet-0136b4bdf9dec20d1",
+      "subnet-0380664ab66dcbe35"
+    ]
   }
 }
 
 resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = aws_subnet.eks_subnet[*].id
+  node_role_arn   = "arn:aws:iam::047719656762:role/eks-role"
+  subnet_ids      = [
+    "subnet-0670bc5b424180d00",
+    "subnet-01d3f9c3ffd4694a4",
+    "subnet-0136b4bdf9dec20d1",
+    "subnet-0380664ab66dcbe35"
+  ]
   desired_size    = 3
   max_size        = 5
   min_size        = 1
   instance_type   = "t3.medium"
+
+  # Ensures the node group waits for the cluster to be fully created
+  depends_on = [aws_eks_cluster.my_cluster]
+
+  tags = {
+    Name = "my-eks-node-group"
+  }
 }
+
