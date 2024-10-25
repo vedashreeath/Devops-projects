f0ca636f8a4bd074f07681bc33b929f90a99fa3e
vedashreeath <36235092+vedashreeath@users.noreply.github.com>
2024-10-25 17:08:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,6 @@ resource "aws_eks_cluster" "my_cluster" {
   vpc_config {
     subnet_ids = [
       "subnet-0670bc5b424180d00",
-      "subnet-01d3f9c3ffd4694a4",
-      "subnet-0136b4bdf9dec20d1",
       "subnet-0380664ab66dcbe35"
     ]
   }
@@ -21,8 +19,6 @@ resource "aws_eks_node_group" "my_node_group" {
   node_role_arn   = "arn:aws:iam::047719656762:role/eks-role"
   subnet_ids      = [
     "subnet-0670bc5b424180d00",
-    "subnet-01d3f9c3ffd4694a4",
-    "subnet-0136b4bdf9dec20d1",
     "subnet-0380664ab66dcbe35"
   ]
   desired_size    = 3
